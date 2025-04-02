24e7ed73dafe977ac3d77f80e09f864265e49423
piramonpan <piramonti@gmail.com>
2025-04-01 20:35:32 -0700

    code part 2 - WIP

--- part2.py
+++ part2.py
@@ -5,14 +5,54 @@ import threading
 import queue
 import time, random
 
-def consumerWorker (queue):
+def consumerWorker (queue : queue.Queue):
     """target worker for a consumer thread"""
-    # to implement  
-  
-def producerWorker(queue):
+    def waitForItemToBeConsumed() -> None: #inner function; use as is
+        time.sleep(round(random.uniform(.1, .3), 2)) #a random delay (100 to 300
+
+    for _ in range(SIZE * 2): #we just consume twice the buffer size for testing
+        #write the code below to correctly remove an item from the circular buffer
+        waitForItemToBeConsumed() #wait for the item to be consumed
+        item = queue.get()
+        print(f"DEBUG: {item} consumed")
+        
+def producerWorker(queue : queue.Queue):
     """target worker for a producer thread"""
-    # to implement
+
+    def waitForItemToBeProduced() -> int: #inner function; use as is
+        time.sleep(round(random.uniform(.1, .3), 2)) #a random delay (100 to 300ms)
+        return random.randint(1, 99) #an item is produced
+    
+    for _ in range(SIZE * 2):
+        item = waitForItemToBeProduced()
+        print(f"DEBUG: {item} produced" )
+        queue.put(item=item)
+        print(list(queue.queue))
+
 
 if __name__ == "__main__":
+    SIZE = 5
+    THREADS = 5
+
     buffer = queue.Queue()
-    # to implement
+
+    # Creating producer and consumer threads 
+    producer_threads : list[threading.Thread] = []
+    consumer_threads : list[threading.Thread] = []
+
+    for _ in range(5):
+        producer_threads.append(threading.Thread(target=producerWorker, args=(buffer,)))
+    for _ in range(4):
+        consumer_threads.append(threading.Thread(target=consumerWorker, args=(buffer,), daemon=True))
+
+    for i in producer_threads:
+        i.start()
+    for j in consumer_threads:
+        j.start()
+        
+    for i in producer_threads:
+        i.join()
+    for j in consumer_threads:
+        j.join()
+
+    print("All items produced and consumed succesfully")
